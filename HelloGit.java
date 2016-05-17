ce06ddb700cb09261ebd975926ba8e9c0c8a5112
curt furumark <curt.r.truc@gmail.com>
2016-05-17 10:08:00 +0200

    new feature sayGoodBye

--- HelloGit.java
+++ HelloGit.java
@@ -1,7 +1,11 @@
 public class HelloGit{
-	public static final String VERSION ="01";
+	public static final String VERSION ="0.2";
+	public static void sayGoodBye(){
+		System.out.println("sayGoodBye()");
+	}
 	public static void main(String[] args){
 		System.out.println("HelloGit: " + VERSION);
+		sayGoodBye();
 
 	}
 }
